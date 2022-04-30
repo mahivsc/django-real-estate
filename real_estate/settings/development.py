19f47f9dca4882487194b7a6b1c8b9c043a32fb5
mahi <mahi.django@gmail.com>
2022-04-30 20:32:50 +0530

    logging info  configured

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -3,8 +3,8 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': env('POSTGRES_ENGINE'),
-        'NAME': env('POSTGRES_NAME.DB'),
-        'USER': env('POSTGRES_USER'),
+        'NAME': env('POSTGRES_NAME'),
+        'USER':  env('POSTGRES_USER'),
         'PASSWORD':env('POSTGRES_PASSWORD'),
         'HOST':env('POSTGRES_HOST'),
         'PORT':env('POSTGRES_PORT'),
